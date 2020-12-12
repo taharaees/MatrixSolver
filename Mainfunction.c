c6e5f04432e566c1119859a0140411049c6a6a97
taharaees <taha.raees13@gmail.com>
2020-12-12 17:52:16 +0500

    updated the inputting process of single or dual matrices

--- Mainfunction.c
+++ Mainfunction.c
@@ -2,31 +2,52 @@
 void main()
 {
 	// initialisation of variables
-	int arr1[10][10], arr2[10][10], i, j, r1, c1, r2, c2, op;
-	// Taking input from user, rows and columns of first matrices
-	printf("\nEnter number of rows and colums for first matrices respectively:");
-	scanf("%d" "%d", &r1, &c1);
-	// Taking input from user, elements of first matrices
-	for (i=0; i<r1; ++i)
-	{
-		for(j=0; j<c1; ++j)
-		{
-			printf("\nEnter elements of first matrices %drow, %dcol:", i, j);
-			scanf("%d", &arr1[i][j]);
-		}
+	int arr1[10][10], arr2[10][10], i, j, r1, c1, r2, c2, op, n;
+	printf("Enter number of matrices u want to enter 1 or 2:");
+	scanf("%d", &n);
+	// Checking how many matrices and prompting input according to that
+	if (n==1)
+	{    // if a single matrice is inputted
+		printf("\nEnter number of rows and colums for the matrices respectively:");
+	    scanf("%d" "%d", &r1, &c1);
+	    // Taking input from user, elements of the matrices
+	    for (i=0; i<r1; ++i)
+	    {
+		    for(j=0; j<c1; ++j)
+		    {
+			    printf("\nEnter elements of the matrices %drow, %dcol:", i, j);
+			    scanf("%d", &arr1[i][j]);
+		    }
+	    }
 	}
-	// Taking input from user, rows and columns of second matrices
-	printf("\nEnter number of rows and colums for second matrices respectively:"); 
-	scanf("%d" "%d", &r2, &c2);
-	// Taking input from user, elements of second matrices
-	for (i=0; i<r2; ++i)
-	{
-		for(j=0; j<c2; ++j)
-		{
-			printf("\nEnter elements of second matrices %drow, %dcol:", i, j);
-			scanf("%d", &arr2[i][j]);
-		}
+	else
+	{    //if 2 matrices are inputted
+		// Taking input from user, rows and columns of first matrices
+	    printf("\nEnter number of rows and colums for first matrices respectively:");
+	    scanf("%d" "%d", &r1, &c1);
+	    // Taking input from user, elements of first matrices
+	    for (i=0; i<r1; ++i)
+	    {
+		    for(j=0; j<c1; ++j)
+		    {
+			    printf("\nEnter elements of first matrices %drow, %dcol:", i, j);
+			    scanf("%d", &arr1[i][j]);
+		    }
+	    }
+	    // Taking input from user, rows and columns of second matrices
+	    printf("\nEnter number of rows and colums for second matrices respectively:"); 
+	    scanf("%d" "%d", &r2, &c2);
+	    // Taking input from user, elements of second matrices
+	    for (i=0; i<r2; ++i)
+	    {
+	 	    for(j=0; j<c2; ++j)
+		    {
+			    printf("\nEnter elements of second matrices %drow, %dcol:", i, j);
+			    scanf("%d", &arr2[i][j]);
+		    }
+	    }
 	}
+	
 	// Taking input from user, operation he wants to do with matrices
 	printf("\nChoose the number for the corresponding operation between two matrices");
 	printf("\n0: Add");
