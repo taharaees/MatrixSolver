b55792f9859ac188d8f8255937debf1ca8948b4f
taharaees <taha.raees13@gmail.com>
2020-12-14 21:33:57 +0500

    Added the Unit matrix function

--- Mainfunction.c
+++ Mainfunction.c
@@ -54,69 +54,112 @@ int transpose(int r, int c, int arr[10][10]) // By Muhammad Taha Raees
         }
     }
 }
-void main() // By Muhammad Taha Raees
+int unit(int r, int c)  // By Muhammad Taha Raees
+{
+	int i, j, resarr[r][r];
+	for(i=0; i<r; ++i)
+	{
+		for(j=0; j<c; ++j)
+		{
+			if (i==j)
+			{
+				resarr[i][j]=1;
+			}
+			else
+			{
+				resarr[i][j]=0;
+			}
+		}
+	}
+	printf("\nUnit matrix of the desired dimensions is: \n");
+	for(i=0; i<r; ++i)
+    {
+        for (j=0; j<c; ++j)
+		{
+            printf("%d   ", resarr[i][j]);
+            if (j==c-1)
+			{
+                printf("\n");
+            }
+        }
+    }
+}
+void main()      // By Muhammad Taha Raees
 {
 	// initialisation of variables
 	int r1, c1, r2, c2, arr1[10][10], arr2[10][10], i, j, op, n;
-	printf("Enter number of matrices u want to enter 1 or 2:");
-	scanf("%d", &n);
-	// Checking how many matrices and prompting input according to that
-	if (n==1)
-	{    // if a single matrice is inputted
+	char ch;
+	// Asking if user wants to produce a unit matrix
+	printf("Do you want to produce a unit matrix? (y/n)");
+	scanf("%c", &ch);
+	if (ch=='y')
+	{
 		printf("\nEnter number of rows and colums for the matrices respectively:");
 	    scanf("%d" "%d", &r1, &c1);
-	    // Taking input from user, elements of the matrices
-	    for (i=0; i<r1; ++i)
-	    {
-		    for(j=0; j<c1; ++j)
-		    {
-			    printf("\nEnter elements of the matrices %drow, %dcol:", i, j);
-			    scanf("%d", &arr1[i][j]);
-		    }
-	    }
+		op=10;
 	}
 	else
-	{    //if 2 matrices are inputted
-		// Taking input from user, rows and columns of first matrices
-	    printf("\nEnter number of rows and colums for first matrices respectively:");
-	    scanf("%d" "%d", &r1, &c1);
-	    // Taking input from user, elements of first matrices
-	    for (i=0; i<r1; ++i)
-	    {
-		    for(j=0; j<c1; ++j)
-		    {
-			    printf("\nEnter elements of first matrices %drow, %dcol:", i, j);
-			    scanf("%d", &arr1[i][j]);
-		    }
+	{
+		printf("Enter number of matrices u want to enter 1 or 2:");
+	    scanf("%d", &n);
+	    // Checking how many matrices and prompting input according to that
+	    if (n==1)
+	    {    // if a single matrice is inputted
+		    printf("\nEnter number of rows and colums for the matrices respectively:");
+	        scanf("%d" "%d", &r1, &c1);
+	        // Taking input from user, elements of the matrices
+	        for (i=0; i<r1; ++i)
+	        {
+		        for(j=0; j<c1; ++j)
+		        {
+			        printf("\nEnter elements of the matrices %drow, %dcol:", i, j);
+			        scanf("%d", &arr1[i][j]);
+		        }
+	        }
 	    }
-	    // Taking input from user, rows and columns of second matrices
-	    printf("\nEnter number of rows and colums for second matrices respectively:"); 
-	    scanf("%d" "%d", &r2, &c2);
-	    // Taking input from user, elements of second matrices
-	    for (i=0; i<r2; ++i)
-	    {
-	 	    for(j=0; j<c2; ++j)
-		    {
-			    printf("\nEnter elements of second matrices %drow, %dcol:", i, j);
-			    scanf("%d", &arr2[i][j]);
-		    }
+	    else
+	    {    //if 2 matrices are inputted
+		    // Taking input from user, rows and columns of first matrices
+	        printf("\nEnter number of rows and colums for first matrices respectively:");
+	        scanf("%d" "%d", &r1, &c1);
+	        // Taking input from user, elements of first matrices
+	        for (i=0; i<r1; ++i)
+	        {
+		        for(j=0; j<c1; ++j)
+		        {
+			        printf("\nEnter elements of first matrices %drow, %dcol:", i, j);
+			        scanf("%d", &arr1[i][j]);
+		        }
+	        }
+	        // Taking input from user, rows and columns of second matrices
+	        printf("\nEnter number of rows and colums for second matrices respectively:"); 
+	        scanf("%d" "%d", &r2, &c2);
+	        // Taking input from user, elements of second matrices
+	        for (i=0; i<r2; ++i)
+	        {
+	 	        for(j=0; j<c2; ++j)
+		        {
+			        printf("\nEnter elements of second matrices %drow, %dcol:", i, j);
+			        scanf("%d", &arr2[i][j]);
+		        }
+	        }
 	    }
+	
+	    // Taking input from user, operation he wants to do with matrices
+	    printf("\nChoose the number for the corresponding operation between two matrices");
+	    printf("\n0: Add");
+	    printf("\n1: Subtract");
+	    printf("\n2: Multiply");
+	    printf("\n3: Conjugate");
+	    printf("\n4: Inverse");
+	    printf("\n5: Determinant(only for 2 by 2 matrix)");
+ 	    printf("\n6: Skew Hemition");
+	    printf("\n7: Transpose");
+	    printf("\n8: Singular");
+	    printf("\n9: Hemition");
+	    scanf("%d", &op);
 	}
 	
-	// Taking input from user, operation he wants to do with matrices
-	printf("\nChoose the number for the corresponding operation between two matrices");
-	printf("\n0: Add");
-	printf("\n1: Subtract");
-	printf("\n2: Multiply");
-	printf("\n3: Conjugate");
-	printf("\n4: Inverse");
-	printf("\n5: Determinant(only for 2 by 2 matrix)");
-	printf("\n6: Unit matrix");
-	printf("\n7: Transpose");
-	printf("\n8: Singular");
-	printf("\n9: Hemition");
-	printf("\n10: Skew Hemition\n");
-	scanf("%d", &op);
 	// checking conditions, if condition meets, call function
 	switch (op)
 	{
@@ -181,7 +224,7 @@ void main() // By Muhammad Taha Raees
 			
 			break;
 		case 10:
-			
+			unit(r1, c1);
 			break;
 		default:
 			printf("no such operation exists");
