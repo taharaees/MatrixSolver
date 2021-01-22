3b4591568f7aa2313c637aafabdc86463bcf7c54
AnumAamir <75771650+AnumAamir@users.noreply.github.com>
2021-01-22 20:42:11 +0500

    Update Quiz.c

--- Quiz.c
+++ Quiz.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+struct student
+{
+	char roll[20];
+	char name[50];
+};
 void main()
 {
+	struct student s;
 	FILE *fp;
 	int sol1[2][2], sol2[2][2], sol3[2][2];
-	char name[50];
-	char roll[20];
-	fp= fopen("quiz.txt", "r+");
-	fscanf(fp, "%s", name);
-	fgets(name, 20, fp);
+	fp= fopen("quiz.txt", "r");
+	if(fp == NULL)
+   {
+      printf("Error!");   
+      exit(1);             
+   }
+
+	fseek(fp, 6, SEEK_SET);
+	fgets(s.name, 50, fp);
+	fgets(s.roll, 20, fp);
 	fclose(fp);
-	printf("%s", name);
+	puts(s.name);
+	puts(s.roll);
+	
 }
