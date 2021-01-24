840874d0f8074a322ef6c2a4acd26c640cd15e90
AnumAamir <75771650+AnumAamir@users.noreply.github.com>
2021-01-24 10:29:03 +0500

    Update Mainfunction.c

--- Mainfunction.c
+++ Mainfunction.c
@@ -30,7 +30,7 @@ void symmetric(int r1,int c1,int arr1[10][10])  // Anum Aamir
         printf("This matrix is a Symmetric matrix: \n");
 
         for(i=0;i<r1;i++)
-        {  
+        { 
             for(j=0;j<c1;j++)
             {
                 printf("%d  ", arr1[i][j]);
@@ -318,7 +318,7 @@ void main()      // By Muhammad Taha Raees
 			}
 			break;
 		case 2:
-			if(n==1)
+			if(r1==c1 && r2==c2)
 			{
 				multiply(arr1, arr2, r1, c1, r2, c2);//function call
 			}	
